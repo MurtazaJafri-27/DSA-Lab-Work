3d2a5465b43badf9867985cfbca1c1f1d9ff2bab
Syed Murtaza Husain Jafri <2512319@LAB.SZABIST.NET.PK>
2026-09-25 10:31:14 +0500

    second class

--- testing.java
+++ testing.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class testing {
     public static void main(String args[]){
         // int[] arr = { 10, 20, 30 };
@@ -56,4 +59,80 @@ public class testing {
         }
         return arr;
     }
+
 }
+
+class Shopping {
+    static int[] shrinkIfNeeded(int[] cart, int size){
+        if(size < cart.length / 4){
+            int[] smaller = new int[cart.length / 2];
+            System.arraycopy(cart, 0, smaller, 0, size);
+            smaller = cart;
+            return smaller;
+            
+        }
+        return cart;
+    }
+}
+
+class expand {
+    static int[] scaleIfNeeded(int[] arr, int size){
+        if(size < arr.length / 2){
+            int[] bigger = new int[arr.length * 2];
+            System.arraycopy(bigger, 0, arr, 0, size);
+            bigger = arr;
+
+            return bigger;
+        }
+        return arr;
+    }
+
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(5);
+
+        System.out.println(list);
+
+        ArrayList<String> fruits = new ArrayList<>();
+
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.remove("Apple");
+
+        System.out.println(fruits);
+
+        ArrayList<Integer> nums = new ArrayList<>();
+
+        System.out.println(nums.size());
+
+        for(int i = 0; i < 15; i++){
+            nums.add(i);
+        }
+
+        for(int j= 0; j < 4; j++){
+            nums.remove(j);
+        }
+
+        System.out.println(nums.size());
+
+        ArrayList<String> names = new ArrayList<>(List.of("Ali","Sara","Murtaza"));
+
+        System.out.println(names.get(1));
+
+        names.set(1, "Zara");
+        System.out.println(names);
+
+        ArrayList<Integer> newNums = new ArrayList<>(5);
+
+        System.out.println(newNums.size());
+
+        for(int i = 0; i < 10; i++){
+            newNums.add(i);
+        }
+
+        System.out.println(newNums.size());
+    }
+    }
+
